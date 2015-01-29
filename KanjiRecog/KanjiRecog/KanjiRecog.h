69ae4473a5f524f25978120c4bcca659381e5860
Charles Stebbins <cstebs@gmail.com>
2015-01-29 12:29:57 -0600

    Added Static Library project.

---
+++ KanjiRecog/KanjiRecog/KanjiRecog.h
@@ -0,0 +1,13 @@
+//
+//  KanjiRecog.h
+//  KanjiRecog
+//
+//  Created by SOLOMO on 1/29/15.
+//  Copyright (c) 2015 codeandthings. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KanjiRecog : NSObject
+
+@end
---
+++ KanjiRecog/KanjiRecog/KanjiRecog.m
@@ -0,0 +1,13 @@
+//
+//  KanjiRecog.m
+//  KanjiRecog
+//
+//  Created by SOLOMO on 1/29/15.
+//  Copyright (c) 2015 codeandthings. All rights reserved.
+//
+
+#import "KanjiRecog.h"
+
+@implementation KanjiRecog
+
+@end
